846c94476ab5d1d4c3088b9e57be4493c96cb488
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-02 20:32:00 -0300

    :sparkles:	feat: add Providers no layout para envelopar a page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Providers } from '@/providers'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
