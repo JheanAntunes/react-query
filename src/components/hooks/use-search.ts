e44f1d1b165ce9868764215c9534b84fd1b040af
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-07 20:04:51 -0300

    :sparkles:	feat: add fn HandlerOfSearchParams

--- src/components/hooks/use-search.ts
+++ src/components/hooks/use-search.ts
@@ -1,11 +1,23 @@
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 
+type HandlerOfSearchParamsProps = {
+  key: string
+  value: string
+}
+
 const useSearch = () => {
   const searchParams = useSearchParams()
   const params = new URLSearchParams(searchParams)
   const pathname = usePathname()
   const { replace } = useRouter()
-  return { params, pathname, replace }
+  const HandlerOfSearchParams = ({
+    key,
+    value
+  }: HandlerOfSearchParamsProps) => {
+    params.set(key, value)
+    replace(`${pathname}?${params.toString()}`)
+  }
+  return { params, pathname, replace, HandlerOfSearchParams }
 }
 
 export default useSearch
