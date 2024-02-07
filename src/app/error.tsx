98dd3bf80de1df50602242f1b8aa24ce4dfe058e
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-02-07 20:18:50 -0300

    :sparkles:	feat: add Error boundary in page home

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,6 +1,33 @@
-'use client'
-const Error = () => {
-  return <div>Error</div>
-}
+'use client' // Error components must be Client Components
 
-export default Error
+import { Button } from '@nextui-org/button'
+import { useEffect } from 'react'
+
+export default function Error({
+  error,
+  reset
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <div className="flex w-full items-center justify-center">
+      <h2 className="font-sans text-3xl md:text-4xl lg:text-5xl">
+        Ocorreu algo errado!
+      </h2>
+      <Button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Fazer uma nova tentativa.
+      </Button>
+    </div>
+  )
+}
